66a1f0b377939b9ba4d2afdb0165ac721b77dcaf
Leslie Bresnahan <les.bres.2@gmail.com>
2014-01-13 20:09:04 -0500

    Update Card.java

--- War/Card.java
+++ War/Card.java
@@ -1,8 +1,26 @@
 public class Card {
 
-       private String _suit, _face;
+    private String _suit, _face;
+    // 4 suits are clubs, hearts, spades, diamonds
+    // _face is String version of value esp important for royalty where for example val=11 -> face=jack
 
-       public Card( String suit, String face) {
-       	      _suit  = suit;
-	      _face = face;
+    private int _value;
+    // numerical value, useful for seeing which card "beats another"
+
+    public Card( String suit, int value) {
+	_suit  = suit;
+	_face = face;
+    }
+
+    public String getSuit() {
+	return _suit;
+    }
+
+    public String getFace() {
+	return _face;
+    }
+	
+
+
+
 }
