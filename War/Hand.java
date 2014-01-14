04849e4cac73b9ececd67e8555420d633d8c699c
Leslie Bresnahan <les.bres.2@gmail.com>
2014-01-13 20:09:51 -0500

    Update Hand.java

--- War/Hand.java
+++ War/Hand.java
@@ -1,5 +1,16 @@
+
 public class Hand {
 
-       private int size;
+    private int _size;
+    
+    private ArrayList<Card> _cards;
 
-       
+    public Hand() {
+
+	_size = 0;
+	_cards = new ArrayList<Card>();
+
+    }
+
+    public void draw( Deck mainDeck, int num ) {
+	
