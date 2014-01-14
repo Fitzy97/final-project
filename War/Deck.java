44defab950308a171bb4a1a9774f06b9d7db7b45
Leslie Bresnahan <les.bres.2@gmail.com>
2014-01-13 20:09:31 -0500

    Update Deck.java

--- War/Deck.java
+++ War/Deck.java
@@ -1,5 +1,4 @@
 
-
 public class Deck {
         
 	private ArrayList<Card> _deck;
@@ -9,7 +8,21 @@ public class Deck {
 
 	       // fill deck with complete set of cards
 
-	       for ( int i < 
+	       for ( int i = 2; i < 15; i++ ) {
+		   Card add1 = new Card("hearts", i);
+		   Card add2 = new Card("spades", i);
+		   Card add3 = new Card("diamonds", i);
+		   Card add4 = new Card("clubs", i);
+
+		   _deck.add( add1 );
+		   _deck.add( add2 );
+		   _deck.add( add3 );
+		   _deck.add( add4 );
+
+	       }
+	}
+}
+		   
 
 	       
 
