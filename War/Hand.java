93b5a55690887abb81df11a087216ae7091e063f
Leslie Bresnahan <les.bres.2@gmail.com>
2014-01-13 21:01:37 -0500

    Update Hand.java

--- War/Hand.java
+++ War/Hand.java
@@ -1,9 +1,15 @@
+// class hand- the collection of cards a player holds
 
 public class Hand {
+	
+	
+// ~~~~~~~~~~~~~Instance Vars~~~~~~~~~~~~~~
 
     private int _size;
     
     private ArrayList<Card> _cards;
+
+// ~~~~~~~~~~~~~Constructor~~~~~~~~~~~~~~
 
     public Hand() {
 
@@ -12,5 +18,21 @@ public class Hand {
 
     }
 
-    public void draw( Deck mainDeck, int num ) {
-	
+// ~~~~~~~~~~~~~Methods~~~~~~~~~~~~~~
+
+    public void take( Deck mainDeck, int num ) {
+    
+    	for (int n = 0; n < num; n ++) {
+    		_cards.add( mainDeck.draw() );
+    	}
+    }
+    
+    
+    public Card draw() {
+              
+    	Card retCard = _cards.get(0);
+        _deck.remove(0);
+        return retCard;
+    }
+}
+    
