92bd2eb5db0bb266beba784a1cb46dfa6ddbd485
Leslie Bresnahan <les.bres.2@gmail.com>
2014-01-15 14:47:39 -0500

    Update GoFish.java

--- GoFish/GoFish.java
+++ GoFish/GoFish.java
@@ -1,5 +1,4 @@
 
-
 // class gofish for minigame
 
 public class GoFish {
@@ -10,43 +9,101 @@ public class GoFish {
   int _difficulty
   
   Deck _deck;
-  Hand _playerH, _compH;
+  Hand _playerH, _compH; _playerP, _compP;
+  // set aside hands for active cards & completed pairs
   
+  
+  //~~~~~~~~~~~CONSTRUCTOR~~~~~~~~~~~~~~~
   public GoFish( int difficulty) {
     
     _difficulty = difficulty;
     
   }
+   // to be inplemented later: difficulty translates to # of cards drawn
   
   
+  //~~~~~~~~~~SETUP METHOD~~~~~~~~~~~
   public void begin() {
     
     _deck = new Deck();
     _playerH = new Hand();
     _compH = new Hand();
+    _playerP = new Hand();
+    _compP = new Hand();
     
     _deck.shuffle();
     
     _playerH.take( _deck, 5);
     _compH.take(_deck, 5)
-    }
   }
   
-  public void reportHand() {
+
+
+// ~~~~~~~~~~~METHOD FOR EACH ROUND OF PLAY~~~~~~~
+  public void round() {
+    reportHand();
+    personGuess();
+    
+  }
+  
+//~~~~~~~~~~~METHODS CENTRAL TO ROUND~~~~~~~~~~~~~
+
+ public void personGuess() {
+    int choice;
+    
+    System.out.print(Enter the number of the card you would like to inquire about:)
+    try {
+      choice = Integer.parseInt( )
+    }
+    
+    int pos = finder( _compH, _playerH.get(choice) );
+    
+    if ( pos > -1) {
+      
+      // add new card to player hand
+      _playerH.add( _compH.draw( pos) );
+      
+      System.out.println("Computer: Yes I have that card... *forks it over*" );
+    } 
+    
+  }
+ 
+ // ~~~~~~~~~~~HELPER METHODS FOR ROUND()~~~~~~~~~
+ 
+    public void reportHand() {
     System.out.println( "Your hand is...");
     
     for (int i = 0; i < _playerH.getSize(); i++) {
-      System.out.println( _playerH.getHand().get( i ) );
+      System.out.println( "Card " + i + "is " _playerH.getHand().get( i ) );
     }
     
   }
   
-  public void round() {
-    reportHand();
-    
+  // returns first index where target appears or -1 if not present
+  public int finder( ArrayList<Card> al, Card target) {
+    for (int i = 0; i < al.size(); i++) {
+      if (al.get(i).equals( target ) ) {
+        return i
+      }
+    }
+    return -1;
   }
-  
-  // to be inplemented later: difficulty translates to # of cards drawn
+ 
+ // checks for pairs in a hands and moves them to pairs hand 
+  public void checkPairs( ArrayList<Card> al, ArrayList<Card> pairs ) {
+    
+    for (int i = 0; i < alC.size(); i++) {
+      Card checker = alC.get(i);
+      for (int n = 0; n < alC.size(); n++ ) {
+        if ( n != i && checker.equals( alC.get(n)) ) {
+          // prob w/ n and i affecting each other
+          pairs.add( al.draw( i) );
+          
+        }
+      }
+    }
+  }
+
   
   public static void main( String[] args) {
     
