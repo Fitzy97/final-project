d9b456c9c9e3c079df344d78e7dac36ba3574a8d
les-bres <les.bres.2@gmail.com>
2014-01-18 20:58:54 -0500

    go fish

---
+++ DriverGo.java
@@ -0,0 +1,8 @@
+public class DriverGo {
+
+    public static void main( String[] args ) {
+	
+	GoFish goFish = new GoFish( 5 );
+	goFish.play();
+    }
+}
--- GoFish.java
+++ GoFish.java
@@ -12,10 +12,10 @@ public class GoFish {
     int _difficulty, _roundCnt;
   
     Deck _deck;
-    Hand _playerH, _compH; _playerP, _compP;
+    Hand _playerH, _compH, _playerP, _compP;
     // set aside hands for active cards & completed pairs
 
-    Boolean playerT, compT;
+    Boolean _playerT, _compT;
     // booleans for whose turn it is
 
     
@@ -24,12 +24,21 @@ public class GoFish {
     public GoFish( int difficulty) {
     
 	_difficulty = difficulty;
-    
+
+    	_deck = new Deck();
+	_playerH = new Hand();
+	_compH = new Hand();
+	_playerP = new Hand();
+	_compP = new Hand();
+
+	_playerT = true;
+	_compT = false;
     }
     // to be inplemented later: difficulty translates to # of cards drawn
   
     //~~~~~~~~~~PLAY METHOD~~~~~~~~~~~~
     public void play() {
+	System.out.println("Welcome to go fish!");
 	begin();
 	while ( _deck.getDeck().size() > 0 ) {
 	    round();
@@ -43,11 +52,6 @@ public class GoFish {
     //~~~~~~~~~~SETUP METHOD~~~~~~~~~~~
     public void begin() {
     
-	_deck = new Deck();
-	_playerH = new Hand();
-	_compH = new Hand();
-	_playerP = new Hand();
-	_compP = new Hand();
     
 	_deck.shuffle();
     
@@ -65,53 +69,59 @@ public class GoFish {
     // ~~~~~~~~~~~METHOD FOR EACH ROUND OF PLAY~~~~~~~
     public void round() {
 
-	if (roundCnt > 0 ) {
-	    reportHand();
-	}
+	//	if (_roundCnt > 0 ) {
+	//    reportHand();
+	//}
+
 
 	// method based on whose turn it is
-	if ( personT ) {
+	if ( _playerT ) {
 	    personGuess();
 	}
-	else if ( compT ) {
+	else if ( _compT ) {
+	    computerGuess();
 	    
 	}
-	roundCnt ++;
+	_roundCnt ++;
     
     }
   
     //~~~~~~~~~~~METHODS CENTRAL TO ROUND~~~~~~~~~~~~~
 
-    public boolean personGuess() {
+    public void personGuess() {
+
+	System.out.println("\n------------------------------");
+	System.out.println("PLAYER'S TURN");
+	System.out.println("------------------------------");
 
 	int choice;
 	boolean readGuess = true;
 
+	reportHand();
+	choice = getInt( "Enter the number of the card you would like to inquire about" );
 
 	while (readGuess) {
-	    System.out.print(Enter the number of the card you would like to inquire about:);
-	    try {
-		choice = Keyboard.readInt();
-	    }
-	    catch( DataException e) {
-		System.out.println( "Oops, invalid input, try again." );
-	    }
-	    if (choice < 1 || choice > _playerH.getCards.size() ) {
+	    if (choice < 1 || choice > _playerH.getCards().size() ) {
 		System.out.println( "Oops, you don't have any cards with that corresponding number, try again." );
+		choice = getInt( "Enter the number of the card you would like to inquire about" );
 	    }
 	    else {
 		readGuess = false;
 	    }
 	}
 
-	    
-	Card cardWanted = _playerH.get( choice );
+	choice --;
+	Card cardWanted = _playerH.getCards().get( choice );
+	System.out.println("Player: Do you have any " + cardWanted.getFace() + "s?");
+
 	int pos = finder( _compH, cardWanted );
     
 	if ( pos > -1) {
 	    // add new card to player hand
 	    _playerH.add( _compH.draw( pos) );
-	    System.out.println("Computer: Yes I have that card... *forks it over*" );
+	    System.out.println("Computer: Yes... *forks it over*" );
+	    checkPairs( _playerH, _playerP, "Player" );
+
 	    System.out.println("It's now your turn again!");
 	} 
 	else {
@@ -120,48 +130,47 @@ public class GoFish {
 	    Card newCard = _deck.draw();
 	    System.out.println( "You drew a " + newCard.getFace() );
 	    _playerH.add( newCard );
+	    checkPairs( _playerH, _playerP, "Player" );
 
 	    if ( newCard.equals( cardWanted ) ) {
-		checkPairs( _playerH, _playerP, "Player" );
 		System.out.println( "You asked for that card, so you get to go again!" );
 	    }
 	    else {
-		personT = false;
-		computerT = true;
+		_playerT = false;
+		_compT = true;
 	    }
 	}
-	
     }
  
  
     public void computerGuess() {
-	    
-	boolean readAns = true;
-	int choice = (int) (math.random() * _compH.size());
-	Card choiceC = _compH.get(choice);
+
+	System.out.println("\n------------------------------");
+	System.out.println("COMPUTER'S TURN");
+	System.out.println("------------------------------");	    
+
+	int choice = (int) (Math.random() * _compH.getCards().size());
+	Card choiceC = _compH.getCards().get(choice);
 
 	    
-	System.out.println("Computer: Do you have any " choiceC.getFace() + "s?");
+	System.out.println("Computer: Do you have any " + choiceC.getFace() + "s?");
 	reportHand();
 
 	int ans = readAns();
-	impletementAns( choice C, ans );
+	implementAns( choiceC, ans );
    
-	}
+    }
 
-	// ~~~~~~~~~~~HELPER METHODS FOR ROUND()~~~~~~~~~
+    // ~~~~~~~~~~~HELPER METHODS FOR ROUND()~~~~~~~~~
  
     public int readAns() {
+	boolean readAns = true;
+	int ans = getInt( "Enter 1 if yes, enter 2 if no." );
+
 	while (readAns ) {
-	    System.out.print("Enter 1 if yes, enter 2 if no.");
-	    try {
-		int ans = Keyboard.readInt();
-	    }
-	    catch (DataException e) {
-		System.out.println( "Oops, invalid input, try again." );
-	    }
-	    if ( choice != 1 || choice != 2 ) {
+	    if ( ans != 1 && ans != 2 ) {
 		System.out.println( "Oops, your answers must be 1 or 2, try again." );
+		ans = getInt( "Enter 1 if yes, enter 2 if no." );
 	    }
 	    else {
 		readAns = false;
@@ -171,91 +180,124 @@ public class GoFish {
     }
 
 
-	// read player's confirmation about whether or not they have 
-	public void implementAns( Card choiceC, int ans ) {
+    // read player's confirmation about whether or not they have 
+    public void implementAns( Card choiceC, int ans ) {
   
-	    int pos = finder( _playerH, choiceC );
-	    boolean needsLoop = true;
+	int pos = finder( _playerH, choiceC );
+	boolean needsLoop = true;
 
-	    while ( needsLoop ) {
-		if (ans == 1) {
-		    if (pos > -1) {
-			System.out.println("Now the computer gets to go again.");
-			checkPairs( compH, compP, "Computer" );
-			_compH.add( _playerH.draw(pos) );
-			needsLoop = false;
+	while ( needsLoop ) {
+	    if (ans == 1) {
+		if (pos > -1) {
+		    System.out.println("Now the computer gets to go again.");
+		    checkPairs( _compH, _compP, "Computer" );
+		    _compH.add( _playerH.draw(pos) );
+		    needsLoop = false;
+		}
+		else {
+		    System.out.println( "Hey, you are a liar, try again!" );
+		    ans = readAns();
+		}
+	    }
+	    else if ( ans == 2 ) {
+		if (pos == -1 ) { 
+		    needsLoop = false;
+		    System.out.println( "Player: Go Fish!" );
+
+		    Card newCard = _deck.draw();
+		    _compH.add( newCard );
+		    checkPairs( _compH, _compP, "Computer" );
+
+		    if ( newCard.equals( choiceC ) ) {
+			System.out.println( "The card drawn is what was asked for, so the computer gets to go again." );
 		    }
 		    else {
-			System.out.println( "Hey, you are a liar, try again!" );
-			ans = readAns();
-			break;
+			_playerT = true;
+			_compT = false;
 		    }
 		}
-		else if ( ans == 2 ) {
-		    if (pos == -1 ) { 
-			System.out.println( "Player: Go Fish!" );
-
-			Card newCard = _deck.draw();
-			_compH.add( newCard );
-
-			if ( newCard.equals( choiceC ) ) {
-			    System.out.println( "The card drawn is what was asked for, so the computer gets to go again." );
-			}
-			else {
-			    personT = true;
-			    computerT = false;
-		    }
+		else {
+		    System.out.println( "Hey, you are a liar, try again!" );
+		    ans = readAns();
 		}
- 
-		public void reportHand() {
-		    System.out.println( "Your hand is...");
-    
-		    for (int i = 1; i <= _playerH.getCards.getsize(); i++) {
-			System.out.println( "Card " + i + "is " _playerH.getHand().get( i ) );
-		    }
-    
-		}
-  
-		// returns first index where target appears or -1 if not present
-		public int finder( ArrayList<Card> al, Card target) {
-		    for (int i = 0; i < al.size(); i++) {
-			if (al.get(i).equals( target ) ) {
-			    return i;
-			}
-		    }
-		    return -1;
-		}
- 
-		// checks for pairs in a hands and moves them to pairs hand 
-		public void checkPairs( Hand hand, Hand handP, String name ) {
-    
-		    ArrayList<Card> handCard = hand.getCards()
-
-			for (int i = 0; i < alC.size(); i++) {
-			    Card checker = alC.get(i);
-
-			    for (int n = 0; n < alC.size(); n++ ) {
-				if ( n != i && checker.equals( alC.get(n)) ) {
-
-				    Card type = HandCards.get( n );
-				    System.out.println( name + " has a pair of " + type.getFace() + "s" );
-
-				    if ( n > i ) {
-					HandP.add( hand.draw( n ) );
-					HandP.add( hand.draw( i ) );
-				    }
-				    else {
-					HandP.add( hand.draw( i ) );
-					HandP.add( hand.draw( n ) );
-				    }
-				}
-			    }
-			}
-		}
-    
-
-    
-    
-		}
-  
 	    }
+	}
+    }
+ 
+    public void reportHand() {
+	System.out.println( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" );
+	System.out.println( "Your hand is...");
+	
+	ArrayList<Card> cards = _playerH.getCards();
+    
+	for (int i = 0; i < cards.size(); i++) {
+	    System.out.println( "Card " + (i + 1) + " is the " + cards.get( i ) );
+	}
+	System.out.println( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" );
+    
+    }
+  
+    // returns first index where target appears or -1 if not present
+    public int finder( Hand hand, Card target) {
+
+	ArrayList<Card> al = hand.getCards();
+
+	for (int i = 0; i < al.size(); i++) {
+	    if (al.get(i).equals( target ) ) {
+		return i;
+	    }
+	}
+	return -1;
+    }
+ 
+    // checks for pairs in a hands and moves them to pairs hand 
+    public void checkPairs( Hand hand, Hand handP, String name ) {
+    
+	ArrayList<Card> alC = hand.getCards();
+
+	for (int i = 0; i < alC.size(); i++) {
+	    Card checker = alC.get(i);
+
+	    for (int n = 0; n < alC.size(); n++ ) {
+		if ( n != i && checker.equals( alC.get(n)) ) {
+
+		    Card type = alC.get( n );
+		    System.out.println( name + " has a pair of " + type.getFace() + "s" );
+
+		    if ( n > i ) {
+			handP.add( hand.draw( n ) );
+			handP.add( hand.draw( i ) );
+		    }
+		    else {
+			handP.add( hand.draw( i ) );
+			handP.add( hand.draw( n ) );
+		    }
+		    if (name.equals("Player")) {
+			reportHand();
+		    }
+		}
+	    }
+	}
+    }
+
+	public int getInt(String message) {
+	    boolean readInt = true;
+	    int num = -1;
+
+	    while (readInt) {
+		System.out.println(message);
+		try {
+		    num = Keyboard.readInt();
+		    readInt = false;
+		}
+		catch ( InputMismatchException e ) {
+		    System.out.println("Oops, invalid input, try again.");
+		}
+	    }
+
+	    return num;
+	}
+
+    }
+  
+
