78136dbbda2aa26205bf6a614793059434fc7cdd
Fitzy97 <seanfitzgerald321@gmail.com>
2014-01-22 14:37:25 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -15,6 +15,14 @@ public class TicTacToe {
 	isr = new InputStreamReader( System.in );
 	in = new BufferedReader( isr );
     }
+    
+    public static void pause(int seconds){
+        Date start = new Date();
+        Date end = new Date();
+        while(end.getTime() - start.getTime() < seconds * 1000){
+         end = new Date();
+        }
+    }
 
     public boolean isThreeInARow( String mark ) {
 
