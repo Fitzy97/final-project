13eb907e1d350cde2f3b96fa710b280de8bb0e42
Fitzy97 <seanfitzgerald321@gmail.com>
2014-01-23 02:48:12 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-public class TicTacToe extends MiniGame {
+public class TicTacToe {
 
-    private int turnCount;
+    private int _difficulty, turnCount;
     private BufferedReader in;
     private InputStreamReader isr;
     private ArrayList<String> board = new ArrayList<String>();
@@ -15,13 +15,89 @@ public class TicTacToe extends MiniGame {
 	isr = new InputStreamReader( System.in );
 	in = new BufferedReader( isr );
     }
-    
+
     public static void pause(int seconds){
         Date start = new Date();
         Date end = new Date();
         while(end.getTime() - start.getTime() < seconds * 1000){
          end = new Date();
         }
+    }
+
+    public int twoInARow( String mark ) {
+
+	int retInt = (int)(Math.random() * 9);
+	for (int i = 0; i < board.size(); i++) {
+	    if (board.get(i).equals(" " + mark + " ")) {
+		if ( (!(i == 2 || i == 5 || i == 8) && board.get(i).equals(board.get(i+1))) ) {
+		    if (i == 0 || i == 3 || i == 6) {
+			retInt = i+2;
+			break;
+		    }
+		    else {
+			retInt = i-1;
+			break;
+		    }
+		}
+		else if ((i == 0 || i == 3 || i == 6) && board.get(i+2).equals(board.get(i))) {
+		    retInt = i + 1;
+		    break;
+		}
+		else if (i < 6 && board.get(i).equals(board.get(i+3))) {
+		    if (i < 3) {
+			if (board.get(i).equals(board.get(i+6))) {
+			    retInt = i + 3;
+			    break;
+			}
+			else {
+			    retInt = i + 6;
+			    break;
+			}
+		    }
+		    else {
+			retInt = i - 3;
+			break;
+		    }
+		}
+		else if (i < 3 && board.get(i).equals(board.get(i+6))) {
+		    retInt = i + 3;
+		    break;
+		}
+		else if (i > 5 && board.get(i).equals(board.get(i-3))){
+		    retInt = i - 6;
+		    break;
+		}
+		else if (i == 4) {
+		    if (board.get(i).equals(board.get(i+4))) {
+			retInt = i - 4;
+			break;
+		    }
+		    else if (board.get(i).equals(board.get(i-4))) {
+			retInt = i + 4;
+			break;
+		    }
+		    else if (board.get(i).equals(board.get(i-2))) {
+			retInt = i + 2;
+			break;
+		    }
+		    else if (board.get(i).equals(board.get(i+2))) {
+			retInt = i - 2;
+			break;
+		    }
+		}
+		else if (i == 0 && board.get(i).equals(board.get(8))) {
+		    retInt = 4;
+		    break;
+		}
+		else if (i == 2 && board.get(i).equals(board.get(6))) {
+		    retInt = 4;
+		    break;
+		}
+	    }
+	}
+		
+	System.out.println("RETINT: " + retInt);
+	return retInt;
     }
 
     public boolean isThreeInARow( String mark ) {
@@ -83,8 +159,10 @@ public class TicTacToe extends MiniGame {
 		s+= "|";
 	}
 	System.out.println(s);
+	pause(1);
 	System.out.println("Pick your mark: X or O");
 	System.out.println("1: X\n2: O");
+	System.out.print("Your choice: ");
 	try {
 	    choice = Integer.parseInt( in.readLine() );
 	}
@@ -98,8 +176,10 @@ public class TicTacToe extends MiniGame {
 	    oppMark = "X";
 	}
 
+	pause(1);
 	System.out.println("Would you like to go first?");
 	System.out.println("1: Yes\n2: No");
+	System.out.print("Your choice: ");
 	try {
 	    choice = Integer.parseInt( in.readLine() );
 	}
@@ -146,6 +226,7 @@ public class TicTacToe extends MiniGame {
 		taken.add(choice);
 		board.remove(choice-1);
 		board.add(choice-1, " " + mark + " ");
+		pause(1);
 	        System.out.println("The Board: ");
 		s = "";
 		for (int i = 0; i < 9; i++) {
@@ -159,20 +240,27 @@ public class TicTacToe extends MiniGame {
 
 		if (moveCount < 4) {
 		    System.out.println("Your opponent is thinking...");
+		    pause(2);
 		    boolean b = true;
 		    while (b) {
-			choice = (int)(Math.random() * 9) + 1;
+			if (_difficulty == 1)
+			    choice = (int)(Math.random() * 9);
+			else if (_difficulty == 3)
+			    choice = twoInARow( mark );
+			else
+			    choice = twoInARow( oppMark );
 			boolean d = false;
 			for (int i = 0; i < taken.size(); i++) {
-			    if (taken.get(i) == choice)
+			    if (taken.get(i) == choice+1)
 				d = true;
 			}
 			b = d;
 		    }
 		    taken.add(choice);
-		    board.remove(choice-1);
-		    board.add(choice-1, " " + oppMark + " ");
+		    board.remove(choice);
+		    board.add(choice, " " + oppMark + " ");
 
+		    pause(1);
 		    System.out.println("The board: ");
 		    s = "";
 		    for (int i = 0; i < 9; i++) {
@@ -202,20 +290,27 @@ public class TicTacToe extends MiniGame {
 	    while (moveCount < 6) {
 
 		System.out.println("Your opponent is thinking...");
+		pause(2);
 		boolean b = true;
 		while (b) {
-		    choice = (int)(Math.random() * 9) + 1;
+		    if (_difficulty == 1)
+			choice = (int)(Math.random() * 9);
+		    else if (_difficulty == 3)
+			choice = twoInARow( mark );
+		    else
+			choice = twoInARow( oppMark );
 		    boolean d = false;
 		    for (int i = 0; i < taken.size(); i++) {
-			if (choice == taken.get(i))
+			if (choice+1 == taken.get(i))
 			    d = true;
 		    }
 		    b = d;
 		}
 		taken.add(choice);
-		board.remove(choice-1);
-		board.add(choice-1, " " + oppMark + " ");
+		board.remove(choice);
+		board.add(choice, " " + oppMark + " ");
 
+		pause(1);
 		System.out.println("The Board: ");
 		s = "";
 		for (int i = 0; i < 9; i++) {
@@ -267,6 +362,7 @@ public class TicTacToe extends MiniGame {
 		    taken.add(choice);
 		    board.remove(choice-1);
 		    board.add(choice-1, " " + mark + " ");
+		    pause(1);
 		    System.out.println("The board: ");
 		    s = "";
 		    for (int i = 0; i < 9; i++) {
