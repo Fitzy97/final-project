d4fafad9959e9647446cbf4713f75a78d57e01f6
Fitzy97 <seanfitzgerald321@gmail.com>
2014-01-24 00:46:09 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -17,62 +17,64 @@ public class Card implements Comparable{
 //~~~~~~~~~~~~~~~~~CONSTRUCTOR~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     public Card( String suit, int value) {
-	_suit  = suit;
-	_value = value;
+        _suit  = suit;
+        _value = value;
     }
 
 //~~~~~~~~~~~~~~~~ACCESSOR METHODS~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     public String getSuit() {
-	return _suit;
+        return _suit;
     }
 
     public String getFace() {
-	return _face;
+        return _face;
+    }
+
+    public int getValue() {
+	return _value;
     }
     
 //~~~~~~~~~~~~~~~OTHER METHODS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
+
     public String toString() {
-    	setFace();
-    	return _face + " of " + _suit;
+	setFace();
+	return _face + " of " + _suit;
     }
+    
     public void setFace() {
-    	if (_value < 11) {
-    		_face = _value + "";
-    	}
-    	else if (_value == 11) {
-    		_face = "jack";
-    	}
-    	else if (_value == 12) {
-    		_face = "queen";
-    	}
-    	else if (_value == 13) {
-    		_face = "king";
-    	}
-    	else {
-    		_face = "ace";
-    	}
+            if (_value < 11) {
+		_face = ((Integer)_value).toString();
+            }
+            else if (_value == 11) {
+		_face = "jack";
+            }
+            else if (_value == 12) {
+		_face = "queen";
+            }
+            else if (_value == 13) {
+		_face = "king";
+            }
+            else {
+		_face = "ace";
+            }
     }
     
     // check if 2 objs are equivalent
     public boolean equals( Object other ) {
-    	return this == other // check for aliases
-    	||
-    	( other instanceof Card 
-    	&& this._value == ((Card) other)._value);
+            return this == other // check for aliases
+            ||
+            ( other instanceof Card 
+            && this._value == ((Card) other)._value);
     }
     
     // tels which of the two Card objs is great
     public int compareTo( Object other ) {
-    	
-    	if (! (other instanceof Card) )
-    		throw new ClassCastException("\ncompareTo() input not Card");
-    	
-    	return this._value - ((Card)other)._value;
+            
+            if (! (other instanceof Card) )
+                    throw new ClassCastException("\ncompareTo() input not Card");
+            
+            return this._value - ((Card)other)._value;
     }
-	
-
-
 
 }
