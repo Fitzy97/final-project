3e3ab58a642735b84927443da8ae173e9d07d7f5
Fitzy97 <seanfitzgerald321@gmail.com>
2014-01-23 20:26:28 -0500

    Update Catz.java

--- Catz.java
+++ Catz.java
@@ -12,7 +12,7 @@ public class Catz {
 
     private int levelCount;
     private boolean gameOver;
-    private int spot;
+    private int spot = 1;
 
 
     private InputStreamReader isr;
@@ -22,11 +22,11 @@ public class Catz {
 
     // ~~~~~~~~~~ DEFAULT CONSTRUCTOR ~~~~~~~~~~~
     public Catz() {
-	levelCount = 0;
-	gameOver = false;
-	isr = new InputStreamReader( System.in );
-	in = new BufferedReader( isr );
-	newGame();
+        levelCount = 0;
+        gameOver = false;
+        isr = new InputStreamReader( System.in );
+        in = new BufferedReader( isr );
+        newGame();
     }
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -35,48 +35,48 @@ public class Catz {
 
     public void displayMap() {
 
-	System.out.println("Opening the map... close it to resume.");
+        System.out.println("Opening the map... close it to resume.");
         ImageIcon image = new ImageIcon("hobbit.jpg");
 
         JLabel label = new JLabel("", image, JLabel.CENTER);
-	label.setLayout(new BorderLayout());
+        label.setLayout(new BorderLayout());
 
         Drawer mapLines = new Drawer(levelCount);
-	mapLines.setOpaque(false);
+        mapLines.setOpaque(false);
 
-	label.add(mapLines);
+        label.add(mapLines);
 
-	JPanel panel = new JPanel(new BorderLayout());
-	panel.add(label, BorderLayout.CENTER);
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(label, BorderLayout.CENTER);
 
-	JFrame frame = new JFrame();
-	frame.add(panel);
-	frame.setSize(1200, 641);
-	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	frame.setVisible(true);
+        JFrame frame = new JFrame();
+        frame.add(panel);
+        frame.setSize(1200, 641);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 
     public void newGame() {
 
-	String s;
-	String name = "";
-	s = "Enter a momentous adventure no one has dared attempt!  Your skills will be tested, but the prize is mighty, should you prevail.  This land may be foreign and full of unfamiliarities... More than your sword shall be tested.  Tread carefully, young wanderer, for you are far from home.\n";
+        String s;
+        String name = "";
+        s = "Enter a momentous adventure no one has dared attempt!  Your skills will be tested, but the prize is mighty, should you prevail.  This land may be foreign and full of unfamiliarities... More than your sword shall be tested.  Tread carefully, young wanderer, for you are far from home.\n";
 
-	System.out.print( s );
+        System.out.print( s );
 
-	s = "You are at the toll gate.  Offer your name: ";
-	System.out.print( s );
+        s = "You are at the toll gate.  Offer your name: ";
+        System.out.print( s );
 
-	try {
-	    name = in.readLine();
-	}
-	catch ( IOException e ) { }
+        try {
+            name = in.readLine();
+        }
+        catch ( IOException e ) { }
 
-	//instantiate the player's character
-	one = new Player( name );
+        //instantiate the player's character
+        one = new Player( name );
 
-	displayMap();
+        displayMap();
 
     }//end newGame()
 
@@ -85,80 +85,96 @@ public class Catz {
       post: Returns true if player wins mini-game.
             Returns false if player loses.
       =============================================*/
-    public int playTurn(
-    	
-	String gameName = "";
-	boolean outcome = true;
-	int diff;
+    public int playTurn() {
+            
+        String gameName = "";
+        boolean outcome = true;
+        int diff;
 
-	if (levelCount < 3)
-	    diff = 1;
-	else if (levelCount < 6)
-	    diff = 2;
-	else
-	    diff = 3;
+        if (levelCount < 3)
+            diff = 1;
+        else if (levelCount < 6)
+            diff = 2;
+        else
+            diff = 3;
 
-	System.out.println("Level " + levelCount);
+        System.out.println("Level " + levelCount);
 
-	if (spot == 1) {
-	    gameName = "Tic-tac-toe";
-	    TicTacToe game = new TicTacToe( diff );
-	    outcome = game.play();}
-	else if (spot == 2) {
-	    gameName = "Mancala";
-	    Mancala game = new Mancala( diff );
-	    outcome = game.play( one );}
-	else if (spot == 3) {
-	    gameName = "Connect Four";
-	    ConnectFour game = new ConnectFour( diff );
-	    outcome = game.play( one );}
-	else if (spot == 4) {
-	    gameName = "Sudoku";
-	    Sudoku game = new Sudoku( diff );
-	    outcome = game.play( one );}
-	else if (spot == 5) {
-	    gameName = "Silo";
-	    Silo game = new Silo( diff );
-	    outcome = game.play( one );}
-	else if (spot == 6) {
-	    gameName = "Go fish";
-	    GoFish game = new GoFish( diff );
-	    outcome = game.play( one );}
-	else if (spot == 7) {
-	    gameName = "Poker";
-	    Poker game = new Poker( diff );
-	    outcome = game.play( one );}
-	else {
-	    gameName = "War";
-	    War game = new War( diff );
-	    outcome = game.play( one );}
-
-	if (outcome) {
-	    System.out.println("You have completed level " + levelCount + ": " + gameName);
-	    spot++;
-
-	    if (spot > 8) {
-	    	System.out.println("You have completed all of level " + levelCount + "!");
-	    	if (diff < 3) {
-	    		Drawer newLine = new Drawer(levelCount);
-	                displayMap();
-	                diff++;
-	                spot = 0;
-	    	}
-	    	else {
-	    		System.out.println("You have beat the game!");
-	    	}
+	while (spot < 10) {
+	    if (spot == 1) {
+		gameName = "Tic-tac-toe";
+		TicTacToe game = new TicTacToe( diff );
+		outcome = game.play();
 	    }
-	}
-	else {
-	    System.out.println("You have been bested! Your health has been reduced 50 points and you cannot pass.");
-	    one.addHealth( -50 );
-	    if ( one.getHealth() <= 0 )
-		return -1;
-	    return 0;
+	    else if (spot == 2) {
+		gameName = "Mancala";
+		Mancala game = new Mancala( diff );
+		outcome = game.play( one );
+	    }
+	    else if (spot == 3) {
+		gameName = "Connect Four";
+		ConnectFour game = new ConnectFour( diff );
+		outcome = game.play( one );
+	    }
+	    else if (spot == 4) {
+		gameName = "Sudoku";
+		Sudoku game = new Sudoku( diff );
+		outcome = game.play( one );
+	    }
+	    else if (spot == 5) {
+		gameName = "Silo";
+		Silo game = new Silo( diff );
+		outcome = game.play( one );
+	    }
+	    else if (spot == 6) {
+		gameName = "Go fish";
+		GoFish game = new GoFish( diff );
+		outcome = game.play( one );
+	    }
+	    else if (spot == 7) {
+		gameName = "Poker";
+		Poker game = new Poker( diff );
+		outcome = game.play( one );
+	    }
+	    else {
+		gameName = "War";
+		War game = new War( diff );
+		outcome = game.play( one );
+	    }
+
+	    if (outcome) {
+		System.out.println("You have completed level " + levelCount + ": " + gameName);
+		spot++;
+
+		if (spot > 8) {
+		    System.out.println("You have completed all of level " + levelCount + "!");
+		    Drawer newLine = new Drawer(levelCount);
+		    displayMap();
+		    spot = 1;
+		}
+
+	    }
+        
+	    else {
+		System.out.println("You have been bested! Your health has been reduced 50 points and you cannot pass.");
+		one.addHealth( -50 );
+		System.out.println("For grading/boredom purposes... do you wish to skip to the next game??  yes/no");
+		System.out.print("Your choice: ");
+		String s;
+		try {
+		    s = in.readLine();
+		}
+		catch (IOException e) {}
+		if (s.equals("yes"))
+		    return 1;
+		if ( one.getHealth() <= 0 )
+		    return -1;
+		return 0;
+	    }
+
 	}
 
-	return 1;
+        return 1;
 
     }//end playTurn()
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -166,20 +182,20 @@ public class Catz {
 
     public static void main(String[] args) {
 
-	Catz game = new Catz();
+        Catz game = new Catz();
 
-	int levels = 0;
+        int levels = 0;
 
-	while( levels < NUM_LEVELS ) {
-	    int a = game.playTurn();
-	    if ( a == -1 ) {
-		System.out.println("Your health has expired!");
-		break;
-	    }
-	    levels += a;
-	}
+        while( levels < NUM_LEVELS ) {
+            int a = game.playTurn();
+            if ( a == -1 ) {
+                System.out.println("Your health has expired!");
+                break;
+            }
+            levels += a;
+        }
 
-	System.out.println( "Your adventure is at an end." );
+        System.out.println( "Your adventure is at an end." );
 
     }//end main
 
